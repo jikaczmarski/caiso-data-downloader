df0e0bbf0c2a8e70faa5af19b98410e7d5005e16
Jesse Kaczmarski <39504924+JesseKaczmarski@users.noreply.github.com>
2023-12-08 15:44:22 -0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,74 @@
+# Download System-wide CAISO Demand and Supply Data
+
+This tool was designed to extract large time series from the [CAISO's Today's Outlook](https://www.caiso.com/TodaysOutlook/Pages/default.aspx) web service. This tool can download the following data in 15-minute intervals:
+
+- Demand (MW)
+    - Day ahead forecast, Hour ahead forecast, Current demand, Demand response (if available)
+- Supply (MW)
+    - Solar, Wind, Geothermal, Biomass, Biogas, Small Hydro, Coal,   Nuclear, Natural Gas, Large Hydro, Batteries, Imports, Other
+
+This is a complete redesign of [](). I used network analysis to identify URLs to call recursively instead of using Selenium for element-based webscraping. This halved the code base and removed Google browser and driver dependencies. It is also scalable to other sources on the [Today's Outlook](https://www.caiso.com/TodaysOutlook/Pages/default.aspx) which I will be adding at a later date (specifically resource adequacey). Enjoy!
+
+# Installation
+
+This is a script and should be run in a virtual environment. The script downloads the file to the directory it is run from. An example for setup is as follows,
+
+1. Download the latest release and extract it to a folder of your choice.
+2. Create a virtual enviornment in that directory location and install the requirements:
+   * `$ python -m venv caiso`
+   * `$ source caiso/bin/activate`, OS dependent. [See documentation](https://docs.python.org/3/library/venv.html#how-venvs-work) for your specific operating system.
+   * `$ (caiso) pip install -r requirements.txt`
+3. Once you have a working envrionment with the required packages, you may run the program as shown below.
+
+# Usage
+
+```
+usage: 
+    python caiso-downloader.py [-h] filename startdate enddate {all,demand,supply}
+
+Downloads demand and supply data by reource type at 15-minute intervals
+
+positional arguments:
+  filename             Output path with .csv extension (e.g. filename.csv)
+  startdate            String input: YYYY-MM-DD
+  enddate              String input: YYYY-MM-DD
+  {all,demand,supply}  Target data: all, demand, supply
+
+options:
+  -h, --help           show this help message and exit
+
+```
+
+# Example
+
+```
+(venv) $ python caiso-downloader.py supply-demand.csv 2021-01-01 2021-01-05 all
+
+Data source: all 
+ Results location: C:\projects\caiso-demand-supply-downloader\supply-demand.csv
+ Sample begins: Jan 01, 2021
+ Sample ends: Jan 05, 2021
+ Sample length: 5 days ( 1440 observations )
+Duplicate time values found in the demand data
+See error log for removed observations details.
+ First values kept.
+
+```
+
+Note: See [Disclaimer 2](#disclaimer-2) on duplicate time value warnings.
+
+# Example Output
+
+| Time          | Day ahead forecast | Hour ahead forecast | Current demand | Solar | Wind | Geothermal | Biomass | Biogas | Small hydro | Coal | Nuclear | Natural gas | Large hydro | Batteries | Imports | Other |
+|---------------|--------------------|---------------------|----------------|-------|------|------------|---------|--------|-------------|------|---------|-------------|-------------|-----------|---------|-------|
+| 1/1/2021 0:00 | 22662              | 21935               | 21937          | -41   | 1424 | 958        | 309     | 199    | 144         | 13   | 1144    | 8069        | 690         | -46       | 9541    | 0     |
+| 1/1/2021 0:05 | 21434              | 21713               | 21858          | -41   | 1449 | 959        | 309     | 201    | 143         | 13   | 1145    | 8084        | 698         | -20       | 9388    | 0     |
+| 1/1/2021 0:10 | 21434              | 21713               | 21827          | -41   | 1430 | 958        | 310     | 202    | 142         | 13   | 1145    | 8077        | 702         | 22        | 9314    | 0     |
+| 1/1/2021 0:15 | 21434              | 21713               | 21757          | -41   | 1430 | 958        | 308     | 202    | 142         | 13   | 1145    | 8050        | 701         | 46        | 9247    | 0     |
+
+### Disclaimer 1
+
+The data from the CAISO is not perfect. There are bound to be several errors here and there. These errors would be present whether or not you downloaded the CSV files manually or through this program. 
+
+### Disclaimer 2
+By default, the program keeps the first occurance of duplicate time entries. Those duplicate entries and their data are output in an error log alongside the CSV.
---
+++ caiso-downloader.py
@@ -0,0 +1,141 @@
+# Import required packages
+import argparse
+from datetime import datetime, timedelta, date
+import os
+import glob
+import pandas as pd
+import csv
+import time
+import sys
+import numpy as np
+import json
+
+# Parser setup
+parser = argparse.ArgumentParser(prog='CAISO System Wide Demand and Supply Downloader',
+                                 description='Downloads demand and supply data by reource type at 15-minute intervals',
+                                 epilog='GNU Affero General Public License v3.0')
+
+## Program argument validation
+def valid_file_inputs(d):
+    if d.endswith(".csv") == True:
+        return d
+    else:
+        filename_error_message = "Not a valid filename input. Missing .csv extension."
+        raise argparse.ArgumentTypeError(filename_error_message)
+
+def valid_date_inputs(s):
+    try:
+        return datetime.strptime(s, "%Y-%m-%d")
+    except ValueError:
+        date_error_message = "Not a valid date input: {0!r}".format(s)
+        raise argparse.ArgumentTypeError(date_error_message)
+
+## Program arguments
+parser.add_argument('filename',
+                    type=valid_file_inputs,
+                    help='The name of the output file with .csv extension (e.g. filename.csv)')
+parser.add_argument('startdate',
+                    type=valid_date_inputs, 
+                    help='String input: YYYY-MM-DD')
+parser.add_argument('enddate',
+                    type=valid_date_inputs, 
+                    help='String input: YYYY-MM-DD')
+parser.add_argument('target',
+                    choices=['all','demand','supply'],
+                    help='Target data: all, demand, supply'
+                    )
+
+## Program argument 
+args = parser.parse_args()
+
+## Recieved program argument validation conditions
+if args.startdate > args.enddate:
+    print('Error: Provided start date exceeds the end date.')
+    exit()
+else:
+    pass
+
+if args.startdate < datetime.strptime('2018-04-10','%Y-%m-%d'):
+    print('Error: Start date exceeds available data. Earliest available data is Apr 10, 2018.')
+    exit()
+else:
+    pass
+
+if args.enddate > (datetime.now() - timedelta(days=1)):
+    print('Error: No data available for dates into the future. You entered:',args.enddate.strftime("%b %d%, %Y"))
+    exit()
+else:
+    pass
+
+input_delta = args.enddate - args.startdate
+
+# File output location
+results_location = os.path.join(sys.path[0], args.filename) # Operating system agnostic
+
+# User argument summary
+print(
+    "Data source:", args.target, os.linesep,
+    "Results location:", results_location, os.linesep,
+    "Sample begins:", args.startdate.strftime("%b %d, %Y"), os.linesep,
+    "Sample ends:", args.enddate.strftime("%b %d, %Y"), os.linesep,
+    "Sample length:", (input_delta.days + 1), "days (", (input_delta.days + 1)*288, "observations )"
+)
+
+# json object to easily add and remove working urls
+urls_json = """
+{
+    "demand": "https://www.caiso.com/outlook/SP/History/datetime/demand.csv",
+    "supply": "https://www.caiso.com/outlook/SP/History/datetime/fuelsource.csv"
+}
+"""
+
+# URL Targets
+if args.target == "supply":
+    url = [json.loads(urls_json)['supply']]
+if args.target == "demand":
+    url = [json.loads(urls_json)['demand']]
+if args.target == "all":
+    url = [json.loads(urls_json)['supply'], json.loads(urls_json)['demand']]
+
+# Initialize 
+df_demand = pd.DataFrame()
+df_supply = pd.DataFrame()
+
+while args.startdate <= args.enddate:
+    datetime = args.startdate.strftime('%Y%m%d')
+    for i in range(len(url)):
+        url_target = url[i].replace("datetime", datetime)
+        tmp = pd.read_csv(url_target)
+        tmp['Time'] = pd.to_datetime(args.startdate.strftime('%m/%d/%Y') + " " + tmp.iloc[:,0], format="%m/%d/%Y %H:%M")
+        if i == 0:
+            df_supply = pd.concat([df_supply, tmp], ignore_index=True)
+        elif i == 1:
+            df_demand = pd.concat([df_demand, tmp], ignore_index=True)
+    args.startdate = args.startdate + timedelta(days=1)
+
+# Collecting and exporting requested data
+if args.target == 'all':
+    df_demand = df_demand.set_index('Time')
+    if df_demand.index.duplicated().any():
+        df_demand_index_duplicated_values = df_demand[df_demand.index.duplicated()]
+        with open('demand_error_log.txt', 'a') as f:
+            df_demand_index_duplicated_values_str = df_demand_index_duplicated_values.to_string()
+            f.write(df_demand_index_duplicated_values_str)
+        print('Duplicate time values found in the demand data\nSee error log for removed observations details.\n First values kept.')
+    df_demand = df_demand[~df_demand.index.duplicated(keep='first')]
+    df_supply = df_supply.set_index('Time')
+    if df_supply.index.duplicated().any():
+        df_supply_index_duplicated_values = df_supply[df_supply.index.duplicated()]
+        with open('supply_error_log.txt', 'a') as f:
+            df_supply_index_duplicated_values_str = df_supply_index_duplicated_values.to_string()
+            f.write(df_supply_index_duplicated_values_str)
+        print('Duplicate time values found in the supply data\nSee error log for removed observations details.\n First values kept.')
+    df_supply = df_supply[~df_supply.index.duplicated(keep='first')]
+    df = pd.concat([df_demand, df_supply], axis=1)
+    df.to_csv(results_location)
+elif df_demand.empty:
+    df_supply.to_csv(results_location,index=False)
+elif df_supply.empty:
+    df_demand.to_csv(results_location,index=False)
+
+quit()
